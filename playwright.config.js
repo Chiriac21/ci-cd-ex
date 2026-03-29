d95dfc611a836f92e4216db0b40417dd8d5f671b
Chiriac21 <chiriac.gabriel21@yahoo.com>
2026-03-29 22:07:20 +0300

    Add new test

--- playwright.config.js
+++ playwright.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
   webServer: {
     command: 'npm start',
     port: 8080,
+    // eslint-disable-next-line no-undef
     reuseExistingServer: !process.env.CI,
   },
 });
--- test/playwright/e2e.spec.js
+++ test/playwright/e2e.spec.js
@@ -5,5 +5,9 @@ describe('Pokedex', () => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  }),
+  test('navigate to particular pokemon page', async ({ page }) => {
+    await page.goto('/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
