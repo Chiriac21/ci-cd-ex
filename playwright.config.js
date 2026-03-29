07d290d8de20da0e1e6d061fab94b1b3a54bdae5
Chiriac21 <chiriac.gabriel21@yahoo.com>
2026-03-29 22:00:19 +0300

    Add playwright

---
+++ playwright.config.js
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './test/playwright',
+
+  use: {
+    baseURL: 'http://localhost:8080',
+    headless: true,
+  },
+
+  webServer: {
+    command: 'npm start',
+    port: 8080,
+    reuseExistingServer: !process.env.CI,
+  },
+});
+
