8d05b13c407d89d4028e9302af3ec19f7fffdae7
Chiriac21 <chiriac.gabriel21@yahoo.com>
2026-03-31 11:53:58 +0300

    Change ver

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const start = async () => {
 }
 
 app.get('/version', (req, res) => {
-  res.send('15') // change this string to ensure a new version deployed
+  res.send('20') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
